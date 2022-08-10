eac1171e1699925ac175be3f8e3581bd317f66c5
paulwuertz <s3336462@msx.tu-dresden.de>
2022-08-10 15:54:11 +0200

    doc: add to board filter page chips with pop-over form to select filters
    Signed-off-by: Paul Würtz <paulwuertz@posteo.de>

--- doc/_extensions/zephyr/boardselector/__init__.py
+++ doc/_extensions/zephyr/boardselector/__init__.py
@@ -126,15 +126,55 @@ def board_load(app: Sphinx) -> Tuple[kconfiglib.Kconfig, Dict[str, str]]:
 class BoardSearchNode(nodes.Element):
     @staticmethod
     def html():
-        return """<div>
-            <div class="chip">
-                John Doe
-            </div>
-            <div class="chip">
-                John Doe
-                <span class="closebtn" onclick="this.parentElement.style.display='none'">&times;</span>
-            </div>
-        <div>"""
+        interesting_props = [
+            { "name": "cpu", "default_selected": True},
+            { "name": "flash", "default_selected": True},
+            { "name": "memory", "default_selected": True},
+            { "name": "timer", "default_selected": True},
+            { "name": "gpio", "default_selected": True},
+            { "name": "rtc", "default_selected": True},
+            { "name": "i2c", "default_selected": True},
+            { "name": "spi", "default_selected": True},
+            { "name": "uart", "default_selected": True},
+            { "name": "usart", "default_selected": True},
+            { "name": "adc", "default_selected": True},
+            { "name": "dac", "default_selected": True},
+            { "name": "pwm", "default_selected": True},
+            { "name": "usb", "default_selected": True},
+            { "name": "ethernet", "default_selected": True},
+            { "name": "can", "default_selected": True},
+            { "name": "timers", "default_selected": True},
+            { "name": "serial", "default_selected": True},
+            { "name": "sdmmc"},
+            { "name": "wdg"},
+            { "name": "quadspi"}
+        ]
+        arches = ["arc", "arm", "arm64", "nios2", "posix", "riscv", "sparc", "x86", "xtensa"]
+
+        default_options = "\n".join(f'''
+            <form id="filter-form-{i["name"]}" class="checkbox_select filter-form chip">
+                <select name="{i["name"].upper()}" id="add-filters" multiple="multiple">
+                    <option value="0">0</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4+">4+</option>
+                </select>
+
+                <input type="submit" />
+            </form>''' for i in interesting_props if "default_selected" in i and i["default_selected"]
+        )
+        filter_options = "\n".join(f'<option data-count="{i}" value="{p["name"]}">{p["name"].upper()}</option>' for i, p in enumerate(interesting_props))
+        return f"""<div>
+            {default_options}
+            <form id="filter-filter-checkboxes" class="checkbox_select">
+                <select name="Add a Board filter" id="add-filters" multiple="multiple">
+                    {filter_options}
+                </select>
+
+                <input type="submit" />
+            </form>
+        </div>"""
 
 
 def board_search_visit_html(self, node: nodes.Node) -> None:
--- doc/_extensions/zephyr/boardselector/static/boardselector.mjs
+++ doc/_extensions/zephyr/boardselector/static/boardselector.mjs
@@ -438,4 +438,286 @@ function setupKconfigSearch() {
         });
 }
 
+var checkbox_select = function(params)
+{
+    // Error handling first
+    // ----------------------------------------------------------------------------------------------------
+    var error = false;
+    console.log(params.selector);
+    // If the selector is not given
+    if(!params.selector) {                                              console.error("selector needs to be defined"); error = true; }
+    // If the selector is not a string
+    if(typeof params.selector != "string") {                            console.error("selector needs to be a string"); error = true; }
+    // If the element is not a form
+    if(!$(params.selector).is("form")) {                                console.error("Element needs to be a form"); error = true; }
+    // If the element doesn't contain a select
+    if($(params.selector).find("select").length < 1) {                  console.error("Element needs to have a select in it"); error = true; }
+    // If the element doesn't contain option elements
+    if($(params.selector).find("option").length < 1) {                  console.error("Select element needs to have an option in it"); error = true; }
+    // If the select element doesn't have a name attribute
+    if($(params.selector).find('select').attr('name') == undefined) {   console.error("Select element needs to have a name attribute"); error = true; }
+
+    // If there was an error at all, dont continue in the code.
+    if(error)
+        return false;
+
+    // ----------------------------------------------------------------------------------------------------
+
+    var that            = this,
+        $_native_form   = $(params.selector),
+        $_native_select = $_native_form.find('select'),
+
+        // Variables
+        selector                = params.selector,
+        select_name             = $_native_select.attr('name').charAt(0).toUpperCase() + $_native_select.attr('name').substr(1),
+        selected_translation    = params.selected_translation   ? params.selected_translation   : "selected",
+        all_translation         = params.all_translation        ? params.all_translation        : "All " + select_name + "s",
+        not_found_text          = params.not_found              ? params.not_found              : "No " + select_name + "s found.",
+        currently_selected      = [],
+        conjunctor              = params.conjunctor             ? params.conjunctor             : " or",
+
+        // Create the elements needed for the checkbox select
+        $_parent_div    = $("<div />")      .addClass("checkbox_select"),
+        $_select_anchor = $("<a />")        .addClass("checkbox_select_anchor")     .text( select_name ),
+        $_search        = $("<input />")    .addClass("checkbox_select_search"),
+        $_submit        = $("<input />")    .addClass("checkbox_select_submit")     .val("Apply") .attr('type','submit') .data("selected", ""),
+        $_dropdown_div  = $("<div />")      .addClass("checkbox_select_dropdown"),
+        $_not_found     = $("<span />")     .addClass("not_found hide")             .text(not_found_text),
+        $_ul            = $("<ul />"),
+
+        updateCurrentlySelected = function(){
+            var selected = [], selected_display_string = "";
+
+            $_ul.find("input:checked").each(
+
+                function(){
+                    selected.push($(this).val());
+                    selected_display_string += $(this).val().toLocaleUpperCase() + ", "
+                }
+            );
+            selected_display_string = selected_display_string.substring(0, selected_display_string.length - 2)
+            let last_comma = selected_display_string.lastIndexOf(",")
+            if (last_comma != -1) {
+                let before_last_comma = selected_display_string.substring(0, last_comma)
+                let after_last_comma = selected_display_string.substring(last_comma + 1)
+                selected_display_string = before_last_comma + conjunctor + after_last_comma
+            }
+
+            currently_selected = selected;
+
+            if(selected.length == 0){
+                $_select_anchor.text( select_name )
+            }
+            else if(selected.length == 1){
+                $_select_anchor.text( selected_display_string + " " + select_name );
+            }
+            else{
+                $_select_anchor.text( selected_display_string + " " + select_name + "s" );
+            }
+        },
+
+        // Template for the li, will be used in a loop.
+        createItem  = function(name, value){
+            var uID             = 'checkbox_select_' + select_name + "_" + name.replace(" ", "_"),
+                $_li            = $("<li />"),
+                $_checkbox      = $("<input />").attr({
+                                            'name'  : name,
+                                            'id'    : uID,
+                                            'type'  : "checkbox",
+                                            'value' : value
+                                        }
+                                    )
+                                    .click(
+
+                                        function(){
+                                            updateCurrentlySelected();
+                                        }
+                                    ),
+
+                $_label         = $("<label />").attr('for', uID),
+                $_name_span     = $("<span />").text(name).prependTo($_label);
+                $_li.append( $_checkbox )
+                $_li.append( $_label )
+            return $_li;
+        },
+
+		apply = function(){
+			$_dropdown_div.toggleClass("show");
+			$_parent_div.toggleClass("expanded");
+
+			if(!$_parent_div.hasClass("expanded")){
+				// Only do the Apply event if its different
+				if(currently_selected != $_submit.data("selected")){
+					$_submit.data("selected" , currently_selected);
+
+					that.onApply({
+                        selected : $_submit.data("selected")
+                    });
+				}
+			}
+		};
+
+    // Event of this instance
+    that.onApply = typeof params.onApply == "function" ?
+
+                    params.onApply :
+
+                    function(e){
+                        //e.selected is accessible
+                    };
+
+    that.update = function(){
+        $_ul.empty();
+        $_native_select.find("option").each(function(i){
+            $_ul.append( createItem( $(this).text(), $(this).val() ) );
+        });
+        updateCurrentlySelected();
+    }
+
+    that.check = function(checkbox_name, checked){
+        //$_ul.find("input[type=checkbox][name=" + trim(checkbox_name) + "]").attr('checked', checked ? checked : false);
+
+		$_ul.find("input[type=checkbox]").each(function(){
+			// If this elements name is equal to the one sent in the function
+			if($(this).attr('name') == checkbox_name){
+				// Apply the checked state to this checkbox
+				$(this).attr('checked', checked ? checked : false);
+
+				// Break out of each loop
+				return false;
+			}
+		});
+
+        updateCurrentlySelected();
+
+    }
+
+    // Build mark up before pushing into page
+    $_dropdown_div  .prepend($_search);
+    $_dropdown_div  .append($_ul);
+    $_dropdown_div  .append($_not_found);
+    $_dropdown_div  .append($_submit);
+    $_dropdown_div  .appendTo($_parent_div);
+    $_select_anchor .prependTo($_parent_div);
+
+    // Iterate through option elements
+    that.update();
+
+    // Events
+
+    // Actual dropdown action
+    $_select_anchor.click(
+
+        function(){
+            apply();
+        }
+    );
+
+    // Filters the checkboxes by search on keyup
+    $_search.keyup(
+
+        function(){
+            var search = $(this).val().toLowerCase().trim();
+
+            if( search.length == 1 ){
+                $_ul.find("label").each(
+
+                    function(){
+                        if($(this).text().toLowerCase().charAt(0) == search.charAt(0)){
+                            if($(this).parent().hasClass("hide"))
+                                $(this).parent().removeClass("hide");
+
+                            if(!$_not_found.hasClass("hide"))
+                                $_not_found.addClass("hide");
+                        }
+                        else{
+                            if(!$(this).parent().hasClass("hide"))
+                                $(this).parent().addClass("hide");
+
+                            if($_not_found.hasClass("hide"))
+                                $_not_found.removeClass("hide");
+                        }
+                    }
+                );
+            }
+            else{
+                // If it doesn't contain
+                if($_ul.text().toLowerCase().indexOf(search) == -1){
+                    if($_not_found.hasClass("hide"))
+                        $_not_found.removeClass("hide");
+                }
+                else{
+                    if(!$_not_found.hasClass("hide"))
+                        $_not_found.addClass("hide");
+                }
+
+                $_ul.find("label").each(
+
+                    function(){
+                        if($(this).text().toLowerCase().indexOf(search) > -1){
+                            if($(this).parent().hasClass("hide"))
+                                $(this).parent().removeClass("hide");
+                        }
+                        else{
+                            if(!$(this).parent().hasClass("hide"))
+                                $(this).parent().addClass("hide");
+                        }
+                    }
+                );
+            }
+        }
+    );
+
+    $_submit.click(
+        function(e){
+            e.preventDefault();
+            apply();
+        }
+    );
+
+    // Delete the original form submit
+    $(params.selector).find('input[type=submit]').remove();
+
+    // Put finalized markup into page.
+    $_native_select.after($_parent_div);
+
+    // Hide the original element
+    $_native_select.hide();
+};
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+    for (let selectCheckbox of $(".filter-form")) {
+        if(selectCheckbox.id) {
+            new checkbox_select({
+                selector : "#"+selectCheckbox.id,
+                selected_translation : "Filters choosen",
+                all_translation : "All filters active",
+                not_found : "No boards matched your filters...",
+
+                // Event during initialization
+                onApply : function(e)
+                {
+                    alert("Custom Event: " + e.selected);
+                }
+            });
+        }
+    }
+
+    new checkbox_select({
+        selector : "#filter-filter-checkboxes",
+        selected_translation : "Filters choosen",
+        all_translation : "All filters active",
+        not_found : "No boards matched your filters...",
+        conjunctor: " and",
+
+        // Event during initialization
+        onApply : function(e)
+        {
+            alert("Custom Event: " + e.selected);
+        }
+    });
+
+});
+
 //setupKconfigSearch();
