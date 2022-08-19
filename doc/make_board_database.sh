e018f5dfdb1eca67d351ee6da37f36b25809cf42
paulwuertz <s3336462@msx.tu-dresden.de>
2022-08-19 23:49:55 +0200

    doc: add script to get all board dts's
    Signed-off-by: Paul Würtz <paulwuertz@posteo.de>

---
+++ doc/make_board_database.sh
@@ -0,0 +1,11 @@
+mkdir -p dts_temp
+west boards > device_list.txt
+
+while IFS= read -r line; do
+  echo west build -d dts_temp/build -p -b $line ../samples/hello_world
+  west build -d dts_temp/build -p -b $line ../samples/hello_world
+  cp dts_temp/build/zephyr/zephyr.dts dts_temp/$line.dts
+done < device_list.txt
+
+rm dts_temp/qemu_*
+rm dts_temp/xt-sim*
